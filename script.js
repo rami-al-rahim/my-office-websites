9699a70377d8c79b13e70f56f5b5f5f967afc863
Rami <rami.ashraf2000@gmail.com>
2025-01-05 19:33:14 +0300

    add new project

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
 
-window.addEventListener('resize', initListPage);
-initListPage()
-
-
-function initListPage() {
-  var target = document.querySelector('.my-projects');
-  if (document.querySelector('body').offsetWidth < 768) {
-    target.style.marginLeft = '0px';
-    return;
-  }
-  // Get the reference element
-  var reference = document.querySelector('.sidebar');
-
-  // Calculate the total offset: width + padding-left
-  var referenceWidth = reference.offsetWidth; // Includes width + padding
-  var referencePadding = parseInt(getComputedStyle(reference).paddingLeft);
-
-  // Set the margin-left of the target element
-  target.style.marginLeft = `${referenceWidth + referencePadding}px`;
-}
 document.getElementById('sub').addEventListener('click', fetchData)
 async function fetchData() {
   const url = 'https://api.rami-dev.com/email/'+ document.getElementById('subInput').value; // The URL to fetch data from
-  const spinner = document.getElementById('loading-spinner');
 
   // Show the loading spinner
-  spinner.style.display = 'block';
   alert('loading... please wait')
   try {
     const response = await fetch(url, {method: "POST"});
